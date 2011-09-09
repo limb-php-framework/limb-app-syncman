b24c41f7702daed65a9f1b03fa76fae2cad98529
Pavel Kislitskiy <kislitskiy.pv@gmail.com>
2011-09-09 17:39:07 +0800

    Update project conf example

--- projects-example/projectA/settings.conf.php
+++ projects-example/projectA/settings.conf.php
@@ -4,7 +4,10 @@ $conf = array(
   'host' => 'myhost.com',
   'user' => 'syncman',
   'key' => '/home/syncman/.ssh/id_dsa',
-  'repository' => 'svn://myrepos/projectA/trunk',
+  'repository' => array(
+    'type' => 'git',
+    'path' => 'myrepos/projectA/',
+  ),
   'remote_dir' => '/var/www/projectA',
   'presync_cmd' => 'php %local_dir%/cli/pre_sync.php',
   'postsync_cmd' => 'ssh -i %key% %user%@%host% \'php %remote_dir%/cli/post_sync.php\'',
