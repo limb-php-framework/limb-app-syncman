c49f9a33bb96cfaf9574bc040aa475a8871a3ae8
r-kitaev <r.kitaev.dev@gmail.com>
2011-10-28 18:48:55 +0400

    Add port to rsync cmd

--- src/model/RSyncProjectSync.class.php
+++ src/model/RSyncProjectSync.class.php
@@ -15,15 +15,20 @@ class RSyncProjectSync extends lmbObject implements ProjectSync
   {
     return $this->_server_conf['user'] . '@' . $this->_server_conf['host'];
   }
+  
+  protected function _getSshPort()
+  {
+    return (isset($this->_server_conf['port'])) ? $this->_server_conf['port'] : '22'; 
+  }
 
   function sync($local_dir, $remote_dir, $sync_opts = null)
   {
     $cmd =
       SYNCMAN_RSYNC_BIN . ' -CvzOrlt --include=tags --include=core' .
-      ' -e "' . SYNCMAN_SSH_BIN . ' -i ' . $this->_server_conf['key'] . '" ' .
+      ' -e "' . SYNCMAN_SSH_BIN . ' -p ' . $this->_getSshPort() . ' -i ' . $this->_server_conf['key'] . '" ' .
       $sync_opts . ' ' . $local_dir . '/ ' . $this->_getRemoteUserWithHost() . ':' . $remote_dir
     ;
-
+    
     return $cmd;
   }
 }
